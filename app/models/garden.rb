bc2900f26c7bb4b089c5f1b27a1e5cb99475aa1a
kevn7n <kevn_nguyen@yahoo.com>
2021-04-27 12:01:30 -0600

    Final submit

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -6,4 +6,8 @@ class Garden < ApplicationRecord
     self.plants.distinct.where("plants.days_to_harvest < ?", 100)
   end
 
+  def most_to_least   #Extension attempt, need to fix count
+    self.plants.select("plants.*, count(plot.plants) as most_used").order(most_used: :desc)
+  end
+
 end
